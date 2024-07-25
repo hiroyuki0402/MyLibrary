a6a7d63e6b57f36a422ac6d466925fc24d877d3c
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2024-07-25 21:30:02 +0900

    [add] 一部の角だけ丸くする拡張を追加

---
+++ Sources/MyLibrary/Views/RoundedCorner.swift
@@ -0,0 +1,18 @@
+#if canImport(UIKit)
+import Foundation
+import SwiftUI
+import UIKit
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+
+
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+
+}
+#endif
